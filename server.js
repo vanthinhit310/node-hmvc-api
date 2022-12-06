b6fc02482f2d89d332174b594e7a332d634ab431
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2022-12-06 14:55:26 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./src/config/baseModel");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to DB
+db.sequelize
+    .sync({ force: true })
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err.message);
+    });
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "NodeJS Rest Api Application." });
+});
+
+// Register router
+// require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
---
+++ src/modules/todos/models/todo.model.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    class Todo extends Model {}
+
+    Todo.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            description: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            deadline: {
+                type: DataTypes.DATEONLY,
+                defaultValue: DataTypes.NOW,
+            },
+            published: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: true,
+            },
+        },
+        { sequelize, modelName: "Todo" }
+    );
+};
